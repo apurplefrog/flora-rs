2bd1ef479c51ddfb58b4caf3666a5f0ad8b37c63
purple frog <147276465+apurplefrog@users.noreply.github.com>
2024-12-13 10:07:39 -0500

    delete target directory

--- target/debug/deps/flora-0e744e2a82736e17.d
+++
@@ -1,7 +0,0 @@
-/home/autumn/flora/target/debug/deps/libflora-0e744e2a82736e17.rmeta: src/main.rs src/command.rs src/compiler.rs
-
-/home/autumn/flora/target/debug/deps/flora-0e744e2a82736e17.d: src/main.rs src/command.rs src/compiler.rs
-
-src/main.rs:
-src/command.rs:
-src/compiler.rs:
--- target/debug/deps/flora-76cccc0bba3f477f.d
+++
@@ -1,9 +0,0 @@
-/home/autumn/flora-rs/target/debug/deps/flora-76cccc0bba3f477f: src/main.rs src/command/mod.rs src/compiler/mod.rs src/compiler/compiler_types.rs src/tokenizer/mod.rs
-
-/home/autumn/flora-rs/target/debug/deps/flora-76cccc0bba3f477f.d: src/main.rs src/command/mod.rs src/compiler/mod.rs src/compiler/compiler_types.rs src/tokenizer/mod.rs
-
-src/main.rs:
-src/command/mod.rs:
-src/compiler/mod.rs:
-src/compiler/compiler_types.rs:
-src/tokenizer/mod.rs:
