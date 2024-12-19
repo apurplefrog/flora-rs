c0f2fcefcae644443352aa4e42bc6257b849baaf
purple frog <theodorebaker08@gmail.com>
2024-12-19 15:09:49 -0500

    add compiler.rs

---
+++ src/compiler/compiler.rs
@@ -0,0 +1,6 @@
+use super::compiler_types::CompilationResult;
+use crate::tokenizer::token_types::AdvancedToken;
+
+pub fn compile(tokens: Vec<AdvancedToken>) -> CompilationResult {
+    CompilationResult::CompilationSuccess
+}
