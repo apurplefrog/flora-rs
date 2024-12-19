9e759c220c7e58e3e3bdb71d2e4884fdc1ca1a98
purple frog <theodorebaker08@gmail.com>
2024-12-19 14:51:58 -0500

    replace if parade with match party

--- src/tokenizer/advance_tokens.rs
+++ src/tokenizer/advance_tokens.rs
@@ -9,68 +9,66 @@ pub fn advance_tokens(basic_tokens: Vec<BasicToken>) -> Vec<AdvancedToken> {
             BasicToken::Operator(operator) => {
                 let mut operator_chars = operator.chars();
                 while let Some(nth_char) = operator_chars.nth(token_char_index) {
-                    if nth_char == ';' {
-                    } else if nth_char == '(' {
-                        advanced_tokens.push(AdvancedToken::OpeningBracket);
-                    } else if nth_char == ')' {
-                        advanced_tokens.push(AdvancedToken::ClosingBracket);
-                    } else if nth_char == '[' {
-                        advanced_tokens.push(AdvancedToken::OpeningSquareBracket);
-                    } else if nth_char == ']' {
-                        advanced_tokens.push(AdvancedToken::ClosingSquareBracket);
-                    } else if nth_char == '{' {
-                        advanced_tokens.push(AdvancedToken::OpeningBrace);
-                    } else if nth_char == '}' {
-                        advanced_tokens.push(AdvancedToken::ClosingBrace);
-                    } else if nth_char == '<' {
-                        if let Some(next_char) = operator_chars.nth(token_char_index) {
-                            if next_char == '=' {
-                                advanced_tokens.push(AdvancedToken::LessThanOrEqual);
+                    match nth_char {
+                        ';' => (),
+                        '(' => advanced_tokens.push(AdvancedToken::OpeningBracket),
+                        ')' => advanced_tokens.push(AdvancedToken::ClosingBracket),
+                        '[' => advanced_tokens.push(AdvancedToken::OpeningSquareBracket),
+                        ']' => advanced_tokens.push(AdvancedToken::ClosingSquareBracket),
+                        '{' => advanced_tokens.push(AdvancedToken::OpeningBrace),
+                        '}' => advanced_tokens.push(AdvancedToken::ClosingBrace),
+                        '<' => {
+                            if let Some(next_char) = operator_chars.nth(token_char_index) {
+                                if next_char == '=' {
+                                    advanced_tokens.push(AdvancedToken::LessThanOrEqual);
+                                }
+                                token_char_index += 1;
+                            } else {
+                                advanced_tokens.push(AdvancedToken::LessThan);
                             }
-                            token_char_index += 1;
-                        } else {
-                            advanced_tokens.push(AdvancedToken::LessThan);
                         }
-                    } else if nth_char == '>' {
-                        if let Some(next_char) = operator_chars.nth(token_char_index) {
-                            if next_char == '=' {
-                                advanced_tokens.push(AdvancedToken::GreaterThanOrEqual);
+                        '>' => {
+                            if let Some(next_char) = operator_chars.nth(token_char_index) {
+                                if next_char == '=' {
+                                    advanced_tokens.push(AdvancedToken::GreaterThanOrEqual);
+                                }
+                                token_char_index += 1;
+                            } else {
+                                advanced_tokens.push(AdvancedToken::GreaterThan);
                             }
-                            token_char_index += 1;
-                        } else {
-                            advanced_tokens.push(AdvancedToken::GreaterThan);
                         }
-                    } else if nth_char == '-' {
-                        if let Some(next_char) = operator_chars.nth(token_char_index) {
-                            if next_char == '>' {
-                                advanced_tokens.push(AdvancedToken::Then);
-                            } else if next_char == '=' {
-                                advanced_tokens.push(AdvancedToken::SubtractAndSet)
+                        '-' => {
+                            if let Some(next_char) = operator_chars.nth(token_char_index) {
+                                if next_char == '>' {
+                                    advanced_tokens.push(AdvancedToken::Then);
+                                } else if next_char == '=' {
+                                    advanced_tokens.push(AdvancedToken::SubtractAndSet)
+                                }
+                                token_char_index += 1;
+                            } else {
+                                advanced_tokens.push(AdvancedToken::Subtract);
                             }
-                            token_char_index += 1;
-                        } else {
-                            advanced_tokens.push(AdvancedToken::Subtract);
                         }
-                    } else if nth_char == '+' {
-                        if let Some(next_char) = operator_chars.nth(token_char_index) {
-                            if next_char == '=' {
-                                advanced_tokens.push(AdvancedToken::AddAndSet);
+                        '+' => {
+                            if let Some(next_char) = operator_chars.nth(token_char_index) {
+                                if next_char == '=' {
+                                    advanced_tokens.push(AdvancedToken::AddAndSet);
+                                }
+                                token_char_index += 1;
+                            } else {
+                                advanced_tokens.push(AdvancedToken::Add);
                             }
-                            token_char_index += 1;
-                        } else {
-                            advanced_tokens.push(AdvancedToken::Add);
                         }
-                    } else if nth_char == '.' {
-                        advanced_tokens.push(AdvancedToken::MemberOf);
-                    } else if nth_char == '"' {
-                        let s = match &basic_tokens[token_index - 1] {
-                            BasicToken::Word(word) => word,
-                            BasicToken::Number(number) => number,
-                            BasicToken::Operator(operator) => operator,
-                        };
-                        advanced_tokens.push(AdvancedToken::String(s.to_string()));
-                    } else {
-                        advanced_tokens.push(AdvancedToken::I8(-3));
+                        '.' => advanced_tokens.push(AdvancedToken::MemberOf),
+                        '"' => {
+                            let s = match &basic_tokens[token_index - 1] {
+                                BasicToken::Word(word) => word,
+                                BasicToken::Number(number) => number,
+                                BasicToken::Operator(operator) => operator,
+                            };
+                            advanced_tokens.push(AdvancedToken::String(s.to_string()));
+                        }
+                        _ => panic!(),
                     }
                     token_char_index += 1;
                 }
