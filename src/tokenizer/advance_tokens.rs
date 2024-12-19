49cc725f73602daccaa876db365a8490f11c2fc9
purple frog <theodorebaker08@gmail.com>
2024-12-19 10:11:25 -0500

    check more advanced tokens

--- src/tokenizer/advance_tokens.rs
+++ src/tokenizer/advance_tokens.rs
@@ -9,7 +9,8 @@ pub fn advance_tokens(basic_tokens: Vec<BasicToken>) -> Vec<AdvancedToken> {
             BasicToken::Operator(operator) => {
                 let mut operator_chars = operator.chars();
                 while let Some(nth_char) = operator_chars.nth(token_char_index) {
-                    if nth_char == '[' {
+                    if nth_char == ';' {
+                    } else if nth_char == '[' {
                         advanced_tokens.push(AdvancedToken::OpeningSquareBracket);
                     } else if nth_char == ']' {
                         advanced_tokens.push(AdvancedToken::ClosingSquareBracket);
@@ -55,17 +56,42 @@ pub fn advance_tokens(basic_tokens: Vec<BasicToken>) -> Vec<AdvancedToken> {
                         } else {
                             advanced_tokens.push(AdvancedToken::Add);
                         }
+                    } else if nth_char == '.' {
+                        advanced_tokens.push(AdvancedToken::MemberOf);
+                    } else if nth_char == '"' {
+                        let s = match &basic_tokens[token_index - 1] {
+                            BasicToken::Word(word) => word,
+                            BasicToken::Number(number) => number,
+                            BasicToken::Operator(operator) => operator,
+                        };
+                        advanced_tokens.push(AdvancedToken::String(s.to_string()));
+                    } else {
+                        advanced_tokens.push(AdvancedToken::I8(-3));
                     }
                     token_char_index += 1;
                 }
             }
 
-            BasicToken::Word(word) => {
-                advanced_tokens.push(AdvancedToken::I8(-1));
-            }
+            BasicToken::Word(word) => match word.as_str() {
+                "include" => advanced_tokens.push(AdvancedToken::Include),
+                "func" => advanced_tokens.push(AdvancedToken::Function),
+                "if" => advanced_tokens.push(AdvancedToken::If),
+                "else" => advanced_tokens.push(AdvancedToken::Else),
+                "while" => advanced_tokens.push(AdvancedToken::While),
+                "var" => advanced_tokens.push(AdvancedToken::Variable),
+                "const" => advanced_tokens.push(AdvancedToken::Const),
+                "ret" => advanced_tokens.push(AdvancedToken::Return),
+                identifier => {
+                    advanced_tokens.push(AdvancedToken::Identifier(identifier.to_string()))
+                }
+            },
 
             BasicToken::Number(number) => {
-                advanced_tokens.push(AdvancedToken::I8(-1));
+                let mut num = String::new();
+
+                for c in number.chars() {}
+
+                advanced_tokens.push(AdvancedToken::I8(-4));
             }
         }
         token_index += 1;
