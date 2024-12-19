edfaa10c9bd7b5037522b8be7ed634321748b477
purple frog <theodorebaker08@gmail.com>
2024-12-19 10:11:07 -0500

    remove basic token debugging

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -9,12 +9,6 @@ use token_types::BasicToken;
 
 pub fn tokenize(chars: Vec<char>) -> Vec<AdvancedToken> {
     let strings = stringify_tokens::stringify_token_chars(chars);
-    println!("[");
-    for s in strings.iter() {
-        println!("\t{:?}", s);
-    }
-    println!("]");
-
     let adv_strings = advance_tokens(strings);
     println!("[");
     for s in adv_strings.iter() {
